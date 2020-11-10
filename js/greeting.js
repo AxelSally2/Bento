434d22867657c7870e2c6cb7e8f30966924c3219
Axel Sally <46726307+AxelSally2@users.noreply.github.com>
2020-11-10 00:07:39 -0500

    fix: hour problem

--- js/greeting.js
+++ js/greeting.js
@@ -7,20 +7,20 @@ console.log(today);
 var name = 'SallSa';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
+var gree1 = 'Go to Sleep! \n';
 var gree2 = 'Hello there ! How are you today ? ';
 var gree3 = 'Good morning! ';
 var gree4 = 'Good afternoon, ';
-var gree5 = '\nGood evening, ';
+var gree5 = 'Good evening, ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 0 && hour < 6) {
     document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 8) {
+} else if (hour >= 6 && hour < 9) {
     document.getElementById('greetings').innerText = gree2;
-} else if (hour >= 9 && hour < 12) {
+} else if (hour >= 9 && hour < 13) {
     document.getElementById('greetings').innerText = gree3 + name;
-} else if (hour >= 12 && hour < 17) {
+} else if (hour >= 13 && hour < 17) {
     document.getElementById('greetings').innerText = gree4 + name;
 } else  {
     document.getElementById('greetings').innerText = gree5 + name;
